dda42e0e6374059ba82e53d2107707020aee5704
aritroCoder <ogcodinggod@gmail.com>
2025-10-12 12:48:03 +0530

    added openrouter

--- src/aislides/config/app_config.py
+++ src/aislides/config/app_config.py
@@ -5,9 +5,12 @@ import os
 load_dotenv()  # Load environment variables from .env file
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-if not GROQ_API_KEY:
-    raise ValueError("GROQ_API_KEY environment variable is not set.")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+if not GROQ_API_KEY and not OPENROUTER_API_KEY:
+    raise ValueError("GROQ_API_KEY and OPENROUTER_API_KEY environment variables are not set.")
 
-MODEL = "openai/gpt-oss-20b"
+OPENROUTER_API_BASE = "https://openrouter.ai/api/v1"
+
+MODEL = "openrouter/google/gemini-2.0-flash-lite-001"
 
 model_provider = GroqProvider(api_key=GROQ_API_KEY, base_url="https://api.groq.com/")
---
+++ src/aislides/core/llm/openrouter.py
@@ -0,0 +1,21 @@
+from src.aislides.config.app_config import (
+    OPENROUTER_API_BASE,
+    OPENROUTER_API_KEY,
+    MODEL,
+)
+import instructor
+
+client = instructor.from_provider(
+    MODEL,
+    base_url=OPENROUTER_API_BASE,
+    api_key=OPENROUTER_API_KEY,
+)
+
+
+async def call_structured_model(messages, response_model):
+    response = await client.chat.completions.create(
+        messages=messages,
+        response_model=response_model,
+        extra_body={"provider": {"require_parameters": True}},
+    )
+    return response
