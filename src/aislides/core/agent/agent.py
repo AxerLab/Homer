f35e71aef7d184ef171d5b048486ba1171823912
Ranit Mukherjee <littleranit@gmail.com>
2025-10-24 23:54:39 +0530

    Fix import statement for prompts and add tex_agent for LaTeX generation

--- src/aislides/core/agent/agent.py
+++ src/aislides/core/agent/agent.py
@@ -2,7 +2,7 @@ from typing import List
 from src.aislides.core.llm import model
 from src.aislides.core.models.presentation.presentation import SlidePresentation
 from pydantic_ai import Agent, NativeOutput
-from .prompts import generator_system_prompt, iterator_system_prompt
+import .prompts
 from src.aislides.core.models.slide.slide import Slide
 
 agent = Agent(
@@ -12,4 +12,8 @@ agent = Agent(
 # agent for iterative slide editing
 interator_agent = Agent(
     model=model, output_type=NativeOutput(List[Slide]), system_prompt=iterator_system_prompt, retries=3
+)
+
+tex_agent = Agent(
+    model=model, output_type=NativeOutput(str), system_prompt=tex_generator_system_prompt, retries=3
 )
